be149de6f0d0b0a4b669cd6727b7c45b67995530
fadyehabamer <fadyamer45@gmail.com>
2022-03-13 23:05:24 +0200

    This i a commit number: 4d03d0f0-a311-11ec-ab03-d7f6f8be2ceb

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+const { v1 } = require('uuid');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+
+const fakeCommits = async () => {
+  for (let index = 1; index <= 5000; index++) {
+    try {
+      let commitMessage = `This i a commit number: ${v1()}`
+
+      await fs.writeFileSync('./commits.txt', `${commitMessage}\n`);
+      await execSync(`git add . && git commit -m"${commitMessage}"`);
+      await delay(50) /// waiting 1 second.
+      console.log(`===================> { ${index} } <=====================`);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  await execSync('git push -u origin master && git push -u origin2 master');
+
+}
+
+fakeCommits();
